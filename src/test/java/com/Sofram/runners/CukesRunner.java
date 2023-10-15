d8269308884250410e5ad538f1f5a760a2bbf6cd
Esra <esracaglar9067@gmail.com>
2023-10-15 22:54:06 +0200

    last

--- src/test/java/com/Sofram/runners/CukesRunner.java
+++ src/test/java/com/Sofram/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Sofram/stepsdefinitions",
         dryRun = false,
-        tags = "@B7SFRM-148"
+        tags = ""
 )
 public class CukesRunner {
 }
